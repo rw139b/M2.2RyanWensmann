a1839b1b499611934f8433cc188b5e65cc56bdf9
rw139b <ryanwensmann@u.boisestate.edu>
2025-01-21 14:23:30 -0700

    added Test coverage/Started changing index my bio

--- test/test.js
+++ test/test.js
@@ -1,50 +1,46 @@
 import {helloWorld, add} from '../js/main.js';
-// Import the sinon library to allow us to create a spy on the console.log function
 import sinon from 'sinon';
 
 QUnit.module('main.js tests', function() {
 
     QUnit.test('helloWorld should print Hello World to the console', function(assert) {
-        //Arrange
         const consoleSpy = sinon.spy(console, 'log');
-        //Act
         helloWorld();
-        //Assert
         assert.ok(consoleSpy.calledWith('Hello World'), 'console.log should be called with Hello World');
         consoleSpy.restore();
     });
 
     QUnit.test('add should return the sum of two numbers', function(assert) {
-        //Arrange
         const num1 = 2;
         const num2 = 3;
         const expected = 5;
-        //Act
         const result = add(num1, num2);
-        //Assert
         assert.equal(result, expected, 'add(2, 3) should return 5');
     });
 
     QUnit.test('add should return the sum of negative numbers', function(assert) {
-        //Arrange
         const num1 = -2;
         const num2 = -3;
         const expected = -5;
-        //Act
         const result = add(num1, num2);
-        //Assert
         assert.equal(result, expected, 'add(-2, -3) should return -5');
     });
 
     QUnit.test('add should return the sum of a positive and a negative number', function(assert) {
-        //Arrange
         const num1 = 2;
         const num2 = -3;
         const expected = -1;
-        //Act
         const result = add(num1, num2);
-        //Assert
         assert.equal(result, expected, 'add(2, -3) should return -1');
     });
 
+    QUnit.test('add should throw an error if inputs are not numbers', function(assert) {
+        assert.throws(() => add('a', 3), /You must provide two numbers to add/, 'add("a", 3) should throw an error');
+        assert.throws(() => add(2, 'b'), /You must provide two numbers to add/, 'add(2, "b") should throw an error');
+    });
+
+    QUnit.test('add should throw an error if inputs are missing', function(assert) {
+        assert.throws(() => add(2), /You must provide two numbers to add/, 'add(2) should throw an error');
+        assert.throws(() => add(), /You must provide two numbers to add/, 'add() should throw an error');
+    });
 });
